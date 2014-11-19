3b4e94ed99e471024fee34437d73512bc5846b41
JustSnow <justsnow17@yandex.ru>
2014-11-19 16:13:59 +0300

    change captcha view render helper

--- config/initializers/simple_captcha.rb
+++ config/initializers/simple_captcha.rb
@@ -1,4 +1,12 @@
 SimpleCaptcha.setup do |sc|
   sc.image_size  = '155x50'
-  sc.image_style = 'distorted_black'
+  sc.image_style = 'stereo_shop_captcha'
+  sc.add_image_style('stereo_shop_captcha',
+    [
+      "-background '#F4F7F8'",
+      "-fill '#000'",
+      "-border 1",
+      "-bordercolor '#83636b'"
+    ]
+  )
 end
