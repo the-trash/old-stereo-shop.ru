686145f46c6f5492a511d8744ebf335d630c962b
JustSnow <justsnow17@yandex.ru>
2015-07-18 13:46:55 +0300

    hotfix for yandex market

--- app/decorators/yandex_market_export_decorator.rb
+++ app/decorators/yandex_market_export_decorator.rb
@@ -4,25 +4,23 @@ class YandexMarketExportDecorator < Draper::Decorator
   def text_format_temp_body
     [].tap do |temp_body|
       temp_body << id
+      temp_body << Settings.yandex_market.text_format.type_name
       temp_body << in_stock
       temp_body << product_url
       temp_body << price_with_discount
       temp_body << Settings.yandex_market.text_format.currency_id
       temp_body << product_category_title
       temp_body << picture_url
-      temp_body << in_stock
-      temp_body << in_stock
-      temp_body << true
+      temp_body << brand_title
       temp_body << title
       temp_body << filtered_description
-      temp_body << brand_title
     end
   end
 
   private
 
   def picture_url
-    h.asset_url photos.default.file.url if photos.any?
+    h.image_url photos.default.file.url if photos.any?
   end
 
   def filtered_description letters_count = Settings.yandex_market.text_format.letters_count
